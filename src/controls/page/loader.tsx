3d4e99c3bdf4725e3be1065aa612636e59bc5ac0
Gary Zhang <gzhang@primeobjects.com>
2022-01-16 19:56:45 -0800

    ^ PageLoader

--- src/controls/page/loader.tsx
+++ src/controls/page/loader.tsx
@@ -3,14 +3,15 @@ import React from 'react';
 import SVG from '../svg';
 
 const PageLoader = (props: Record<string, any>) => {
-    const iconSrc = isNonEmptyString(props.iconSrc)?props.iconSrc:'/logo.svg';
-    const color = isNonEmptyString(props.color)?props.color:'#ff0000';
-    
+    const iconSrc = isNonEmptyString(props.iconSrc) ? props.iconSrc : '/logo.svg';
+    const color = isNonEmptyString(props.color) ? props.color : '#ff0000';
+    const showText = props.showText == true;
+
     return <div className="flex flex-col h-screen my-auto items-center bgimg bg-cover">
-        <div className="border border-gray-100 shadow rounded-xl p-4 max-w-xs w-full m-auto">
+        <div className={`border border-gray-100 rounded-xl p-4 m-auto ${showText ? 'max-w-xs w-full' : ''}`}>
             <div className="flex ">
                 <SVG id="loader_icon" src={iconSrc} color={color} className="spinner h-8 w-8" />
-                <div className="animate-pulse flex-1 space-y-6 py-1 ml-5">
+                {showText && <div className="animate-pulse flex-1 space-y-6 py-1 ml-5">
                     <div className="space-y-3">
                         <div className="grid grid-cols-3 gap-4">
                             <div className="h-2 bg-gray-200 rounded col-span-2"></div>
@@ -18,10 +19,11 @@ const PageLoader = (props: Record<string, any>) => {
                         </div>
                         <div className="h-2 bg-gray-200 rounded"></div>
                     </div>
-                </div>
+                </div>}
             </div>
         </div>
     </div>
 }
+
 
 export default PageLoader;
