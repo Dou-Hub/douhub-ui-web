ebdb9992c8b94d99acd9e14cffecc328496d8df5
Gary Zhang <gzhang@primeobjects.com>
2022-04-07 23:16:38 -0700

    UploadPhotoField autoPreviewResize, disabled

--- src/fields/upload-photo.tsx
+++ src/fields/upload-photo.tsx
@@ -13,7 +13,7 @@ const UploadPhotoField = (props: Record<string, any>) => {
     const defaultValue = isNonEmptyString(props.defaultValue) ? props.defaultValue : null;
     const [value, setValue] = useState('');
     const signedUrlSize: "raw" | 120 | 240 | 480 | 960 | 1440 = !isNil(props.signedUrlSize) ? props.signedUrlSize : 960;
-    const autoPreviewResize = props.autoPreviewResize==true;
+    const autoPreviewResize = props.autoPreviewResize == true;
 
     useEffect(() => {
         const newValue = !isNil(props.value) ? props.value : defaultValue;
@@ -22,8 +22,8 @@ const UploadPhotoField = (props: Record<string, any>) => {
         }
     }, [props.value, defaultValue]);
 
-    const onChange = (newValue: string|null) => {
-        setValue(newValue?newValue:'');
+    const onChange = (newValue: string | null) => {
+        setValue(newValue ? newValue : '');
         if (isFunction(props.onChange)) props.onChange(newValue);
     }
 
@@ -36,16 +36,16 @@ const UploadPhotoField = (props: Record<string, any>) => {
         onChange(null);
     }
 
-    const onChangeUrl = (v:any)=>{
+    const onChangeUrl = (v: any) => {
         onChange(v);
     }
-    
+
     return <div className="flex flex-col w-full" style={wrapperStyle}>
         <LabelField {...(isNil(props.LabelField) ? {} : props)}
             text={label} disabled={disabled} style={labelStyle}
             hidden={!(!hideLabel && (alwaysShowLabel || isNonEmptyString(value)))}
         />
-        <div className="field field-upload field-upload-photo" style={{border: 'none'}}>
+        <div className="field field-upload field-upload-photo" style={{ border: 'none' }}>
             <div className="w-full flex flex-col">
                 <Uploader
                     onRemove={onRemove}
@@ -54,17 +54,21 @@ const UploadPhotoField = (props: Record<string, any>) => {
                     fileType="Photo"
                     entityName={record?.entityName}
                     attributeName={name}
+                    disabled={disabled}
                     recordId={record?.id}
                     label={uploaderLabel}
                     signedUrlSize={signedUrlSize}
                     signedUrlFormat="webp"
                     onSuccess={onSuccessUpload}
                     autoPreviewResize={autoPreviewResize}
-                    wrapperStyle={{ width: '100%', height: 120, ...(isObject(props.uploaderWrapperStyle)?props.uploaderWrapperStyle:{})}} />
-                {allowEditUrl && <Input className="w-full" style={{fontSize:'0.9rem', padding: '2px 4px', 
-                    border: 'dashed 1px rgb(204, 204, 204)', borderTop: 'none'}}
-                value={value} 
-                onChange={onChangeUrl} placeholder="You can also provide url here"/>}
+                    wrapperStyle={{ width: '100%', height: 120, ...(isObject(props.uploaderWrapperStyle) ? props.uploaderWrapperStyle : {}) }} />
+                {allowEditUrl && <Input className="w-full" style={{
+                    fontSize: '0.9rem', padding: '2px 4px',
+                    border: 'dashed 1px rgb(204, 204, 204)', borderTop: 'none'
+                }}
+                    disabled={disabled == true}
+                    value={value}
+                    onChange={onChangeUrl} placeholder="You can also provide url here" />}
                 <NoteField text={note} />
             </div>
         </div>
