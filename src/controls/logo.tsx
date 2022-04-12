3b011d8ea53cc8eede0cfdfec78239eaee7d3f16
Gary Zhang <gzhang@primeobjects.com>
2022-04-11 20:30:50 -0700

    add className to Logo

--- src/controls/logo.tsx
+++ src/controls/logo.tsx
@@ -6,17 +6,17 @@ import { SVG } from 'douhub-ui-web-basic';
 
 const Logo = (props: Record<string, any>) => {
 
-    const { color, id, text, logoStyle } = props;
+    const { color, id, text, logoStyle, textClassName, wrapperClassName, className } = props;
     const iconSize = isNumber(props.iconSize) && props.iconSize > 0 ? props.iconSize : 40;
     const logoSrc = isNonEmptyString(props.logoSrc) ? props.logoSrc : '/logo.svg';
-    const textStyle = {marginLeft: 10, marginRight: 10, alignSelf: 'center', color, fontSize:'1.2rem', fontWeight:700, ...props.textStyle};
+    const textStyle = { marginLeft: 10, marginRight: 10, alignSelf: 'center', color, fontSize: '1.2rem', fontWeight: 700, ...props.textStyle };
 
     const onClick = () => {
         if (isFunction(props.onClick)) props.onClick();
     }
 
-    return <div className="flex justify-start">
-        <div className="flex flex-row"
+    return <div className={`flex justify-start ${isNonEmptyString(wrapperClassName)?wrapperClassName:''}`}>
+        <div className={`flex flex-row ${isNonEmptyString(className)?className:''}`}
             style={isFunction(props.onClick) ? { cursor: 'pointer' } : {}}
             onClick={onClick}>
             {logoSrc.indexOf('.svg') > 0 ?
@@ -28,7 +28,7 @@ const Logo = (props: Record<string, any>) => {
                     src={logoSrc}
                     style={{ width: iconSize, ...logoStyle }}
                 />}
-            {isNonEmptyString(text) && <span style={textStyle}>
+            {isNonEmptyString(text) && <span style={textStyle} className={isNonEmptyString(textClassName)?textClassName:''}>
                 {text}
             </span>}
         </div>
